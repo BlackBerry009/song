c041c879cd0f9d3c6dd4b873e6275f1119d50c78
BlackBerry009 <765042894@qq.com>
2023-07-30 22:51:07 +0800

    cors

--- index.js
+++ index.js
@@ -4,10 +4,10 @@ const bodyParser = require('body-parser')
 const cors = require('cors')
 const song = require('./api/song')
 
+app.use(cors())
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(bodyParser.json())
 app.use(express.json({extend: false}))
-
 app.use('/api/song', song)
 
 const port = 12306
